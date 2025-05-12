a44fd5550f07678266c56ea0d24ae05fb3faf824
Jokubas Karavaitis <jokubaskaravaitis@gmail.com>
2025-05-12 13:00:37 +0300

    burgerrrr

--- about-js/script.js
+++ about-js/script.js
@@ -67,3 +67,12 @@ buttons.forEach((element, counter) => {
     });
 });
 
+function openNav() {
+    document.getElementById("Sidebar").style.width = "250px";
+    document.getElementById("main").style.marginRight = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("Sidebar").style.width = "0px";
+    document.getElementById("main").style.marginLeft= "0px";
+  }
